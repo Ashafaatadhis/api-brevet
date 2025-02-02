ef98d31b736c62c42924749512b103a40c025b9a
ashafaatadhis <skullreaper387@gmail.com>
2025-02-02 11:59:00 +0700

    feat: benerin valdiation datetime

--- validation/kursus.go
+++ validation/kursus.go
@@ -18,8 +18,8 @@ type PostKursus struct {
 	ThumbnailKursus  string    `form:"thumbnail_kursus" validate:"omitempty,url"`
 	StartDate        time.Time `form:"start_date" validate:"required"`
 	EndDate          time.Time `form:"end_date" validate:"required,gtefield=StartDate"`
-	StartTime        string    `form:"start_time" validate:"required"`
-	EndTime          string    `form:"end_time" validate:"required"`
+	StartTime        string    `form:"start_time" validate:"required,datetime=15:04:05"`
+	EndTime          string    `form:"end_time" validate:"required,datetime=15:04:05"`
 
 	HariID []uint        `validate:"required,dive,required" form:"hari_id"`
 	Hari   []models.Hari `form:"hari"`
